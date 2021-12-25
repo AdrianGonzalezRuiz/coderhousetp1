104c8b0e8a73d9375e23faeb65b0510520cbbabf
AdrianGonzalezRuiz <gonzalez.ruiz.adrian@gmail.com>
2021-12-25 20:30:53 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,26 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable jsx-quotes */
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+        Text,
+        View,
+      } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{padding: 150}}>
+      <Text>"Hola Coder"</Text>
+      </View>
+  );
+};
+
+export default App;
